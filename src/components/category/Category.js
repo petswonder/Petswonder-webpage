0cbf458079b1f8c161be90545aa4bcf16f392d71
jeswanthv <jeswanthv001@gmail.com>
2021-04-23 16:03:07 +0530

    snacks->snacks&treats

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,85 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../core/Card'
-import {productByCategory} from '../product/apiProduct'
-import * as icons from '../../images/index'
-import Heading from '../core/Heading'
-import ShoppingCategories from '../shopping/ShoppingCategories'
-import { Link } from 'react-router-dom'
-import food from '../../images/categories/food1.png'
-
+import React, { useEffect, useState } from 'react';
+import Card from '../core/Card';
+import { productByCategory } from '../product/apiProduct';
+import * as icons from '../../images/index';
+import Heading from '../core/Heading';
+import ShoppingCategories from '../shopping/ShoppingCategories';
+import { Link } from 'react-router-dom';
+import food from '../../images/categories/food1.png';
 
 const Category = (props) => {
+  const [data, setData] = useState([]);
+  const icon = icons[`${props.match.params.name}Icon`];
 
-    const [data, setData] = useState([]);
-    const icon = icons[`${props.match.params.name}Icon`];
-
-
-  
-    const getProductByCategory = (name, pet) =>{
-        
-        productByCategory({name, pet})
-        .then(response=>{
-            setData(response)
-        })
-        .catch(err=> {
-            console.log(err);
-        })
-    }
-    useEffect(()=>{
-        const name = props.match.params.name;
-        const pet = props.match.params.pet;
-        getProductByCategory(name, pet);
-        window.scrollTo(0, 0);
-    },[]);
-
+  const getProductByCategory = (name, pet) => {
+    productByCategory({ name, pet })
+      .then((response) => {
+        setData(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
     const name = props.match.params.name;
     const pet = props.match.params.pet;
-    console.log({name})
+    getProductByCategory(name, pet);
+    window.scrollTo(0, 0);
+  }, []);
 
-     const categoryBox = (n, photo, categoryId, color, url) =>{
-        return <div class="col-6 col-sm-2 categorylist">
-            <p class="banner">
-                <Link to ={`/pet/${pet}/category/${name}/${url}`}>
-                    <img alt="Banner 01" class="img-responsive-categ" src={photo} style={{ backgroundColor: `${color}`}} />
-                </Link>
-            </p>
-            <h6><Link to ={`/pet/${pet}/category/${name}/${url}`} class="title" >{n}</Link></h6>
-        </div>
-    }
+  const name = props.match.params.name;
+  const pet = props.match.params.pet;
+  console.log({ name });
 
-    const FoodCategories=()=>{
-        return <div className="container recent-game-section spad set-bg"><div className="row">
-                {categoryBox("Dry Food", food, 1, "#a685e2", "Dry+Food")}
-                {categoryBox("Wet Food", food, 2, "#65d6ce", "Wet+Food")}
-                {categoryBox("Vet Food", food, 3, "#fff76a", "Vet+Food")}
-                {categoryBox("Snacks", food, 4, "#fca3cc", "Snacks")}
-        </div>
-        </div>
-    }
-
+  const categoryBox = (n, photo, categoryId, color, url) => {
     return (
-        <div className="container category col-12 ">
-            <img className="category-img" src={icon} alt=""/>
-            <div className="row">
-                <div className="col-12 col-md-12">
-                    
-                    <Heading text={`Pet ${name}`} />
-                    <br/>
-                    {name==="Food" && FoodCategories()}
-                    <div className="row">
-                        
-                            {data && data.map((p,i)=>(
-                                <div key={i} className="col-xl-3 col-lg-4 col-6 product">
-                                    <Card data={p} />
-                                </div>
-                            ))}
-                        
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-    )
-}
+      <div class='col-6 col-sm-2 categorylist'>
+        <p class='banner'>
+          <Link to={`/pet/${pet}/category/${name}/${url}`}>
+            <img
+              alt='Banner 01'
+              class='img-responsive-categ'
+              src={photo}
+              style={{ backgroundColor: `${color}` }}
+            />
+          </Link>
+        </p>
+        <h6>
+          <Link to={`/pet/${pet}/category/${name}/${url}`} class='title'>
+            {n}
+          </Link>
+        </h6>
+      </div>
+    );
+  };
 
-export default Category
+  const FoodCategories = () => {
+    return (
+      <div className='container recent-game-section spad set-bg'>
+        <div className='row'>
+          {categoryBox('Dry Food', food, 1, '#a685e2', 'Dry+Food')}
+          {categoryBox('Wet Food', food, 2, '#65d6ce', 'Wet+Food')}
+          {categoryBox('Vet Food', food, 3, '#fff76a', 'Vet+Food')}
+          {categoryBox('Snacks&Treats', food, 4, '#fca3cc', 'Snacks&Treats')}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className='container category col-12 '>
+      <img className='category-img' src={icon} alt='' />
+      <div className='row'>
+        <div className='col-12 col-md-12'>
+          <Heading text={`Pet ${name}`} />
+          <br />
+          {name === 'Food' && FoodCategories()}
+          <div className='row'>
+            {data &&
+              data.map((p, i) => (
+                <div key={i} className='col-xl-3 col-lg-4 col-6 product'>
+                  <Card data={p} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/components/shopping/ShoppingCategories.js
+++ src/components/shopping/ShoppingCategories.js
@@ -1,34 +1,40 @@
-import React from 'react'
-import food from '../../images/categories/food1.png'
-import petcare from '../../images/categories/grooming.png'
-import acc from '../../images/categories/accessories1.png'
-import toys from '../../images/categories/toys1.png'
-import gifts from '../../images/categories/clothing.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import food from '../../images/categories/food1.png';
+import petcare from '../../images/categories/grooming.png';
+import acc from '../../images/categories/accessories1.png';
+import toys from '../../images/categories/toys1.png';
+import gifts from '../../images/categories/clothing.png';
+import { Link } from 'react-router-dom';
 
 const ShoppingCategories = (pet) => {
+  console.log(pet);
 
-    
-    console.log(pet.pet);
-
-    const categoryBox = (name, photo, categoryId, color) =>{
-        return <div class="col-6 col-sm-2 categorylist">
-            <p class="banner">
-                <Link to ={`/pet/${pet.pet}/category/${name}`}>
-                    <img alt="Banner 01" class="img-responsive-categ" src={photo} style={{ backgroundColor: `${color}`}} />
-                </Link>
-            </p>
-            <h6><Link to ={`/pet/${pet.pet}/category/${name}`} class="title" >{name}</Link></h6>
-        </div>
-    }
-
-
-
-
+  const categoryBox = (name, photo, categoryId, color) => {
     return (
-        <div className="container recent-game-section spad set-bg">
-            <br/>
-            {/* <div className="colored-heading">
+      <div class='col-6 col-sm-2 categorylist'>
+        <p class='banner'>
+          <Link to={`/pet/${pet.pet}/category/${name}`}>
+            <img
+              alt='Banner 01'
+              class='img-responsive-categ'
+              src={photo}
+              style={{ backgroundColor: `${color}` }}
+            />
+          </Link>
+        </p>
+        <h6>
+          <Link to={`/pet/${pet.pet}/category/${name}`} class='title'>
+            {name}
+          </Link>
+        </h6>
+      </div>
+    );
+  };
+
+  return (
+    <div className='container recent-game-section spad set-bg'>
+      <br />
+      {/* <div className="colored-heading">
                 <div className="row">
                     <div className="col-2">
                         <h2><i class="fas fa-paw"></i></h2>
@@ -42,16 +48,16 @@ const ShoppingCategories = (pet) => {
 
                 </div>
             </div> */}
-     
-            <div className="row">
-                {categoryBox("Food", food, 1, "#a685e2")}
-                {categoryBox("Grooming", petcare, 2, "#65d6ce")}
-                {categoryBox("Accessories", acc, 3, "#fff76a")}
-                {categoryBox("Toys", toys, 4, "#fca3cc")}
-                {categoryBox("Gifts", gifts, 5, "#f6d6ad")}
-            </div>
-        </div>
-    )
-}
 
-export default ShoppingCategories
+      <div className='row'>
+        {categoryBox('Food', food, 1, '#a685e2')}
+        {categoryBox('Grooming', petcare, 2, '#65d6ce')}
+        {categoryBox('Accessories', acc, 3, '#fff76a')}
+        {categoryBox('Toys', toys, 4, '#fca3cc')}
+        {categoryBox('Gifts', gifts, 5, '#f6d6ad')}
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingCategories;
