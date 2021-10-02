4fbe70d2c1fb65e740aa539d5aadfcb4bb57a141
LakshmiVSeelam <lakshmivseelam@gmail.com>
2021-10-02 14:25:22 +0530

    api branch

--- src/components/core/OrderApi.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios';
-
-// export const putOrder = async (body) => {
-//   const response = await axios.post(
-//     `https://petswonder.co.in/petswonder/api/saveOrder/orderDetails`,
-//     { body }
-//   );
-//   return response.data;
-// };
